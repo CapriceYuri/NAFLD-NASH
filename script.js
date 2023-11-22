5dd318d0c465897d6fef9946f543e93e38a80d6d
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-11-21 19:59:45 -0500

    Added access lock

---
+++ script.js
@@ -0,0 +1,14 @@
+const lockContent = document.querySelectorAll(".access");
+const accessKey = document.querySelector('#key');
+const accessKeybtn = document.querySelector('#keyBtn')
+
+accessKeybtn.addEventListener('click', () => {
+    if (accessKey.value === 'crv431') {
+        for (let x = 0; x < lockContent.length; x++) {
+            lockContent[x].style.display = "block"
+        }
+        accessKey.style.display = "none";
+        accessKeybtn.style.display = "none";
+    }
+})
+
