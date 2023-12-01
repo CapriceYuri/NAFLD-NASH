8079d917c74b495a87cd7e5d92d41525eb04b033
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-12-01 18:35:14 -0500

    Added trial numbers

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const lockContent = document.querySelectorAll(".access");
-const accessKey = document.querySelector('#key');
-const accessKeybtn = document.querySelector('#keyBtn')
+// const lockContent = document.querySelectorAll(".access");
+// const accessKey = document.querySelector('#key');
+// const accessKeybtn = document.querySelector('#keyBtn')
 
-accessKeybtn.addEventListener('click', () => {
-    if (accessKey.value === 'crv835') {
-        for (let x = 0; x < lockContent.length; x++) {
-            lockContent[x].style.display = "block"
-        }
-        accessKey.style.display = "none";
-        accessKeybtn.style.display = "none";
-    }
-})
+// accessKeybtn.addEventListener('click', () => {
+//     if (accessKey.value === 'crv835') {
+//         for (let x = 0; x < lockContent.length; x++) {
+//             lockContent[x].style.display = "block"
+//         }
+//         accessKey.style.display = "none";
+//         accessKeybtn.style.display = "none";
+//     }
+// })
 
